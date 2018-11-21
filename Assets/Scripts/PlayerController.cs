1c116cdd9e25093284ebb140eb59c56bac887070
James Zhang <zhangjames389@gmail.com>
2018-11-20 19:21:38 -0800

    First commit w/Main Scene and player object

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        var rb2D = GetComponent<Rigidbody2D>();
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+        rb2D.velocity = movement * speed;
+
+        rb2D.position = new Vector3
+        (
+            Mathf.Clamp(rb2D.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb2D.position.y, boundary.yMin, boundary.yMax)
+        );
+    }
+}
